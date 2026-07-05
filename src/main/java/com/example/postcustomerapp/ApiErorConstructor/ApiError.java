ae303b4e9cb83cf138c1bf576676ce9d0eb321f5
IanGatu <gatuian110@gmail.com>
2026-07-06 01:49:19 +0300

    renamed the api error object constructor

---
+++ src/main/java/com/example/postcustomerapp/ApiErorConstructor/ApiError.java
@@ -0,0 +1,25 @@
+package com.example.postcustomerapp.Apierrorconstructor;
+
+public class ApiError {
+    //private int status;
+    //private String error;
+    private String message;
+
+    public ApiError(String message){
+        //this.status=status;
+        //this.error=error;
+        this.message=message;
+    }
+    
+    /*public int getStatus(){
+        return status;
+    }*/
+
+    /*public String getError(){
+        return error;
+    }*/
+
+    public String getMessage(){
+        return message;
+    } 
+}
