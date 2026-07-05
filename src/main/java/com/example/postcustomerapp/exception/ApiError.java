d78b0614d259c4e22f03aa410942aacea1029d84
IanGatu <gatuian110@gmail.com>
2026-07-05 20:25:45 +0300

    made some ammendments to let spring handle the status code and error message instead of manually using fields

--- src/main/java/com/example/postcustomerapp/exception/ApiError.java
+++ src/main/java/com/example/postcustomerapp/exception/ApiError.java
@@ -1,23 +1,23 @@
 package com.example.postcustomerapp.exception;
 
 public class ApiError {
-    private int status;
+    //private int status;
+    //private String error;
     private String message;
-    private String error;
 
-    public ApiError(int status, String error, String message){
-        this.status=status;
-        this.error=error;
+    public ApiError(String message){
+        //this.status=status;
+        //this.error=error;
         this.message=message;
     }
     
-    public int getStatus(){
+    /*public int getStatus(){
         return status;
-    }
+    }*/
 
-    public String getError(){
+    /*public String getError(){
         return error;
-    }
+    }*/
 
     public String getMessage(){
         return message;
