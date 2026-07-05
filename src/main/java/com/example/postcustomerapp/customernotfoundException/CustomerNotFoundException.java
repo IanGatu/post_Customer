153fb5f57bd47bdab9ae992741fbdd3c05b06309
IanGatu <gatuian110@gmail.com>
2026-07-05 20:15:59 +0300

    changed the structure of the code to let globalexceptionhandler to handle the http response object

--- src/main/java/com/example/postcustomerapp/customernotfoundException/CustomerNotFoundException.java
+++ src/main/java/com/example/postcustomerapp/customernotfoundException/CustomerNotFoundException.java
@@ -1,9 +1,9 @@
 package com.example.postcustomerapp.customernotfoundException;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException{
 
     public CustomerNotFoundException(String message){
