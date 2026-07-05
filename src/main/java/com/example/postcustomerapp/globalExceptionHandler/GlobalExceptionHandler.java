c112a11d6e46994b6c92d9931a0d0c9dfc796011
IanGatu <gatuian110@gmail.com>
2026-07-05 20:29:40 +0300

    used spring httpstatus to automate the status code and error message instead of using manual fields

--- src/main/java/com/example/postcustomerapp/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/postcustomerapp/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,10 +1,23 @@
 package com.example.postcustomerapp.globalExceptionHandler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.postcustomerapp.customernotfoundException.CustomerNotFoundException;
+import com.example.postcustomerapp.exception.ApiError;
 
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ApiError> handleCustomerNotFound(CustomerNotFoundException ex) {
+        ApiError error = new ApiError(ex.getMessage());
     
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
+    
